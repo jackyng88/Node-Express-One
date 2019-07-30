63611d9fea1c624bef9f38068066d17a9526e061
Jacky <jacky.ng9188@gmail.com>
2019-07-30 14:38:33 -0400

    routes

--- app.js
+++ app.js
@@ -9,19 +9,23 @@ const app = express();
 // app gets passed to createServer. The use() allows us to add a new
 // middleware function. It accepts an array of request handlers.
 // next - is a function that will be passed to the below function.
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // allows the request to continue to the next middleware in line.
+
+// This is one way we can add routes by using multiple use() with varying
+// paths.
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" Page</h1>');
+    // Since we are sending a response we don't want to call next()
+
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     // We can still do the nodejs res.write() and res.setHeader() for example,
     // but there are new utility functions that we can do.
     res.send('<h1>Expresssssssssssss!</h1>');
     // send allows us to a response. Allows us to send a body of type any.
     // By default the send method sends a HTML content type.
-
 });
 
 // Instead of how previously we created the server then passed in app, we can just
