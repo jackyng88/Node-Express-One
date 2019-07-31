8904d2981c136a0286da405704ed88f27fb91131
Jacky <jacky.ng9188@gmail.com>
2019-07-31 11:41:27 -0400

    Parsing requests and limiting certain middlware to POST requests only

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //const http = require('http');   // Not necessary with app.listen()
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -10,22 +10,33 @@ const app = express();
 // middleware function. It accepts an array of request handlers.
 // next - is a function that will be passed to the below function.
 
+// This is a necessary middleware to parse request bodys. Without this if we try to say console.log(req.body)
+// it will return undefined. Note - will not parse all types of bodys like files, JSON, etc.
+app.use(bodyParser.urlencoded({extended: true}));
+
 // This is one way we can add routes by using multiple use() with varying
 // paths.
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add Product" Page</h1>');
-    // Since we are sending a response we don't want to call next()
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
+  // Since we are sending a response we don't want to call next()
 
 });
 
+// Note - /product middleware can technically be placed before or after /add-product
+// since it doesn't share anything in common besides '/'.
+// Note - changing app.use() to app.post() will also filter in another way. Previously use() would cause that middleware
+// to trigger on GET requests also, but we only want it on POST.
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    // We can still do the nodejs res.write() and res.setHeader() for example,
-    // but there are new utility functions that we can do.
-    res.send('<h1>Expresssssssssssss!</h1>');
-    // send allows us to a response. Allows us to send a body of type any.
-    // By default the send method sends a HTML content type.
+  // We can still do the nodejs res.write() and res.setHeader() for example,
+  // but there are new utility functions that we can do.
+  res.send('<h1>Expresssssssssssss!</h1>');
+  // send allows us to a response. Allows us to send a body of type any.
+  // By default the send method sends a HTML content type.
 });
 
 // Instead of how previously we created the server then passed in app, we can just
