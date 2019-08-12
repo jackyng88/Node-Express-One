365929063518358d1a454e8bc6d0c85469814ed3
Jacky <jacky.ng9188@gmail.com>
2019-08-12 11:41:33 -0400

    Serving HTML pages with sendFile

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,14 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // Similar to how we use app.use() we can use router.use, or router.get, etc.
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
+  //res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
   // Since we are sending a response we don't want to call next()
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 
- 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Expresssssssssssss!</h1>');
+  // __dirname is a global variable that simply holds the absolute path on our OS to this
+  // project folder. The reason we separate views and shop.html instead of concatenating them
+  // is because of how the path works differently in other OS. Linux uses '/' and Windows
+  // uses '\' for instance. The '../' is to go up one folder since views is a sibling folder.
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 
